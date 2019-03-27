f2a15d0bbe9b0b471a7396ad1822f17f4864fea7
Paul <paul@zom.bi>
2019-03-27 21:15:35 +0100

    Use database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/grunions/coordinator
 
 go 1.12
+
+require github.com/lib/pq v1.0.0
--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	// database driver
+	_ "github.com/lib/pq"
 )
 
-type handler struct{}
+type handler struct {
+	db *sql.DB
+}
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World, %s", r.RemoteAddr)
+	fmt.Fprintf(w, "Hello World, %s\n", r.RemoteAddr)
+
+	var name string
+	rows, err := h.db.Query("SELECT name FROM count")
+	if err != nil {
+		fmt.Fprintf(w, "Error listing DB entries: %s\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Entry: %s\n", name)
+	}
 }
 
 func main() {
 
 	port := os.Getenv("PORT")
-	http.ListenAndServe(":"+port, &handler{})
+	dbrn := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", dbrn)
+	if err != nil {
+		panic(err)
+	}
+
+	http.ListenAndServe(":"+port, &handler{db: db})
 }
