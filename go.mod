e11e5c015ca90ab75c8af1e0ebc7b732fe40aba3
Paul <paul@zom.bi>
2019-03-28 00:07:57 +0100

    Use chi router

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/grunions/coordinator
 
 go 1.12
 
-require github.com/lib/pq v1.0.0
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/lib/pq v1.0.0
+)
--- main.go
+++ main.go
@@ -7,19 +7,47 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+
 	// database driver
 	_ "github.com/lib/pq"
 )
 
-type handler struct {
+type app struct {
 	db *sql.DB
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World, %s\n", r.RemoteAddr)
+func (a *app) Healthcheck(w http.ResponseWriter, r *http.Request) {
+	var success bool
+	err := a.db.QueryRow("SELECT 1 = 1;").Scan(&success)
+
+	if err != nil {
+		fmt.Fprint(w, "Database error\n")
+		return
+	}
+
+	if !success {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Wrong database response\n")
+		return
+	}
+
+	fmt.Fprint(w, "Healthy\n")
+	return
+}
+
+func (a app) Debug(w http.ResponseWriter, r *http.Request) {
+	gameid := chi.URLParam(r, "gameid")
+	modid := chi.URLParam(r, "modid")
+	taskid := chi.URLParam(r, "taskid")
+
+	fmt.Fprintf(w, "gameid:%s modid:%s taskid:%s", gameid, modid, taskid)
+}
+
+func (a app) List(w http.ResponseWriter, r *http.Request) {
 
 	var name string
-	rows, err := h.db.Query("SELECT name FROM count")
+	rows, err := a.db.Query("SELECT name FROM count")
 	if err != nil {
 		fmt.Fprintf(w, "Error listing DB entries: %s\n", err)
 		return
@@ -44,5 +72,32 @@ func main() {
 		panic(err)
 	}
 
-	http.ListenAndServe(":"+port, &handler{db: db})
+	app := &app{
+		db: db,
+	}
+
+	r := chi.NewMux()
+
+	r.HandleFunc("/healthcheck", app.Healthcheck)
+
+	r.Route("/games", func(r chi.Router) {
+		r.Get("/", app.List)
+		r.Route("/{gameid}", func(r chi.Router) {
+			r.Get("/", app.Debug)
+			r.Put("/", app.Debug)
+			r.Route("/mods", func(r chi.Router) {
+				r.Route("/{modid}", func(r chi.Router) {
+					r.Get("/", app.Debug)
+					r.Put("/", app.Debug)
+				})
+			})
+		})
+	})
+
+	r.Route("/tasks", func(r chi.Router) {
+		r.Get("/", app.Debug)
+		r.Delete("/{taskid}", app.Debug)
+	})
+
+	http.ListenAndServe(":"+port, r)
 }
